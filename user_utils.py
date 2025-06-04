9f850befdee5d3d523178b251417f14fbffaaf73
Serhei <s.chikita@mail.ru>
2025-06-04 16:43:20 +0300

    task tests

---
+++ user_utils.py
@@ -0,0 +1,83 @@
+def add(a: int, b: int) -> int:
+    """Сложение двух чисел"""
+    # Тут будет код
+
+def is_even(n: int) -> bool:
+    """Проверка, чётное ли число"""
+    # Тут будет код
+
+
+def max_of_three(a: int, b: int, c: int) -> int:
+    """Максимум из трёх чисел"""
+    # Тут будет код
+
+def reverse_string(s: str) -> str:
+    """Переворачивает строку"""
+    # Тут будет код
+
+
+def factorial(n: int) -> int:
+    """Вычисляет факториал"""
+    # Тут будет код
+
+def is_palindrome(s: str) -> bool:
+    """Проверка, является ли строка палиндромом"""
+    # Тут будет код
+
+def count_vowels(s: str) -> int:
+    """Подсчёт гласных"""
+    # Тут будет код
+
+def fibonacci(n: int) -> list[int]:
+    """Генерация последовательности Фибоначчи длины n"""
+    # Тут будет код
+
+def remove_duplicates(lst: list[int]) -> list[int]:
+    """Удаление дубликатов из списка"""
+    # Тут будет код
+
+def flatten(matrix: list[list[int]]) -> list[int]:
+    """Преобразование матрицы в плоский список"""
+    # Тут будет код
+
+def get_common_elements(a: list[int], b: list[int]) -> list[int]:
+    """Общие элементы двух списков"""
+    # Тут будет код
+
+def is_anagram(a: str, b: str) -> bool:
+    """Проверка, являются ли строки анаграммами"""
+    # Тут будет код
+
+def unique_words(text: str) -> set[str]:
+    """Возвращает уникальные слова из строки"""
+    # Тут будет код
+
+def merge_dicts(d1: dict, d2: dict) -> dict:
+    """Объединяет два словаря"""
+    # Тут будет код
+
+def group_by_parity(lst: list[int]) -> dict[str, list[int]]:
+    """Разделение списка на чётные и нечётные"""
+    # Тут будет код
+
+def transpose(matrix: list[list[int]]) -> list[list[int]]:
+    """Транспонирование матрицы"""
+    # Тут будет код
+
+def validate_password(pwd: str) -> bool:
+    """
+    Проверка пароля по условиям:
+    - не меньше 8 символов
+    - есть цифра
+    - есть заглавная буква
+    """
+    # Тут будет код
+
+def parse_csv_row(row: str) -> list[str]:
+    """Разделяет CSV-строку по запятым, учитывая кавычки"""
+    # Тут будет код
+
+def calculate_expression(expr: str) -> float:
+    """Вычисляет выражение вида '2 + 2 * 3' (без eval)"""
+    # Тут будет код
+
